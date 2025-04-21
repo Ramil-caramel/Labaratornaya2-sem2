b03cbb92ea39b65f0066f648bbf9370f0a2e9a6e
Ramil-caramel <ramilrr2706@gmail.com>
2025-04-21 23:31:02 +0300

    finaly_ABSOLUT

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,12 @@
 #include <iostream>
 
 int main(){
+    /*
     String a = "abcabc",b = "c",c = "qwe",q="";
     std::cout << a << std::endl;
     int i = a == b;
     std::cout <<  i << std::endl;
     return 0;
+    */
+    //I GUST TEST SOMETHING
 }
